44d5ed9ebf85338740d2fdaabfaaa240317926a7
CloudsAreCloudy <>
2022-05-07 21:41:14 +0530

    smooth like butter!!!
    made the scroll smooth

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 function logo() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    window.scrollTo({top: 0, behavior: 'smooth'});
 }
